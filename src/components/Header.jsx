a10d8e1c014b11b5128ece28d9082031b194ce95
Mohaz <107796482+Mohaz24@users.noreply.github.com>
2023-12-24 01:45:11 +1100

    typo fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -81,7 +81,7 @@ function Header() {
               className={({ isActive }) => (isActive ? "active-links" : "")}
               to="partners"
             >
-              Parnters
+              Partners
               <MdOutlineEmojiPeople className="ml-2" />
             </NavLink>
             <NavLink
